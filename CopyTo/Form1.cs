ff47125a9119a10e15ec3effc30606240091a10f
Haqve_vu <novruzluqyds20@gmail.com>
2023-10-30 14:47:31 +0400

    Complete

--- CopyTo/Form1.cs
+++ CopyTo/Form1.cs
@@ -15,11 +15,7 @@ namespace CopyTo
 {
     public partial class Form1 : Form
     {
-        static Thread copyThread;
-        static Thread loadBarThread;
-
-        private bool isSuspended = false;
-        private object suspendLock = new object();
+        static Thread copy_load;
 
 
         public Form1()
@@ -72,61 +68,58 @@ namespace CopyTo
 
         private void btn_copy_Click(object sender, EventArgs e)
         {
-            copyThread = new Thread(CopyFile);
-            loadBarThread = new Thread(LoadBar);
+            copy_load = new Thread(CopyFile);
 
-            copyThread.Start();
-            loadBarThread.Start();
+            copy_load.Start();
         }
 
         private void btn_abort_Click(object sender, EventArgs e)
         {
-            if (copyThread != null && copyThread.IsAlive)
+            if (copy_load != null && copy_load.IsAlive)
             {
-                copyThread.Abort();
-            }
-
-            // Abort the loadBarThread
-            if (loadBarThread != null && loadBarThread.IsAlive)
-            {
-                loadBarThread.Abort();
+                copy_load.Abort();
                 Loading_Bar.Value = 0;
             }
         }
 
         private void btn_suspend_Click(object sender, EventArgs e)
         {
-            lock (suspendLock)
-            {
-                isSuspended = true;
-            }
+            copy_load.Suspend();
         }
 
         private void btn_resume_Click(object sender, EventArgs e)
         {
-            lock (suspendLock)
-            {
-                isSuspended = false;
-                Monitor.PulseAll(suspendLock);
-            }
+            copy_load.Resume();
         }
 
-        private void CopyFile() 
+        private void CopyFile()
         {
-            while (true)
-            {
-                string sourceFilePath = txtbox_from.Text;
+            if (txtbox_from.Text != null && txtbox_to.Text != null)
+
+            { string sourceFilePath = txtbox_from.Text;
 
                 string destinationDirectory = txtbox_to.Text;
 
                 if (File.Exists(destinationDirectory + "\\" + $"{Path.GetFileName(sourceFilePath)}") == false)
                 {
+                    Loading_Bar.Minimum = 0;
+                    Loading_Bar.Maximum = 10;
+                    Loading_Bar.Value = 0;
+
+                    for (int currentValue = 0; currentValue <= 10; currentValue++)
+                    {
+                        Loading_Bar.Value = currentValue;
+                        Application.DoEvents(); // Allow the UI to update
+                        Thread.Sleep(1000); // Sleep to control the speed of filling
+                    }
+
+
 
                     string fileName = Path.GetFileName(sourceFilePath);
 
                     string destinationFilePath = Path.Combine(destinationDirectory, fileName);
 
-                    Thread.Sleep(10000);
+
                     File.Copy(sourceFilePath, destinationFilePath);
 
                 }
@@ -134,39 +127,11 @@ namespace CopyTo
                 {
                     MessageBox.Show("File already exists in this directory", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
-                lock (suspendLock)
-                {
-                    while (isSuspended)
-                    {
-                        Monitor.Wait(suspendLock);
-                    }
-                }
             }
-        }
-        private void LoadBar() 
-        {
-            while (true) 
+            else
             {
-                Loading_Bar.Minimum = 0;
-                Loading_Bar.Maximum = 10;
-                Loading_Bar.Value = 0;
-
-                for (int currentValue = 0; currentValue <= 10; currentValue++)
-                {
-                    Loading_Bar.Value = currentValue;
-                    Application.DoEvents(); // Allow the UI to update
-                    Thread.Sleep(1000); // Sleep to control the speed of filling
-                }
-
-                lock (suspendLock)
-                {
-                    while (isSuspended)
-                    {
-                        Monitor.Wait(suspendLock);
-                    }
-                }
+                MessageBox.Show("You did not mention any path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
-
     }
 }
